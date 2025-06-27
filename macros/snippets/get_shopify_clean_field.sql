adf53922cd6b9eb476b320d3825808b6cded93ab
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-06-27 16:18:40 +0200

    Update get_shopify_clean_field.sql

--- macros/snippets/get_shopify_clean_field.sql
+++ macros/snippets/get_shopify_clean_field.sql
@@ -77,6 +77,9 @@
     {%- elif column_name == 'total_discount' -%}
         {{column_name}} as line_{{column_name}}
 
+    {%- elif column_name == 'price' -%}
+        {{column_name}} as line_{{column_name}}
+
     {%- else -%}
     {{column_name}}
 
