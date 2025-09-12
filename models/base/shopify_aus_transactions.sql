4d3738a55580bd553d3aa61f1a826001d4a5f61f
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-09-12 10:19:09 +0200

    Update shopify_aus_transactions.sql

--- models/base/shopify_aus_transactions.sql
+++ models/base/shopify_aus_transactions.sql
@@ -10,49 +10,89 @@
     "status"
 ] -%}
 
-{%- set shop_selected_fields = [
+{%- set order_selected_fields = [
+    "id",
     "currency"
 ] -%}
 
 {%- set schema_name,
-        table_name, shop_table_name
-        = 'shopify_raw_aus', 'transaction', 'shop' -%}
+        table_name, order_table_name
+        = 'shopify_raw_aus', 'transaction', 'order' -%}
+
+{%- set transaction_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw_aus%', 'transaction') -%}
+{%- set order_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw_aus%', 'order') -%}
 
 WITH 
-    {% if var('sho_aus_currency') == 'USD' -%}
-    shop_raw_data AS 
-    ({{ dbt_utils.union_relations(relations = shop_raw_tables) }}),
-        
-    currency AS
-    (SELECT date, conversion_rate
-    FROM shop_raw_data LEFT JOIN utilities.currency USING (currency)
-    WHERE date <= current_date),
-    {%- endif -%}
+    transaction_raw_data AS 
+    ({{ dbt_utils.union_relations(relations = transaction_raw_tables) }}),
 
-    {%- set conversion_rate = 1 if var('sho_aus_currency') != 'USD' else 'conversion_rate' %}
-    
-    raw_table AS 
+    order_raw_data AS 
+    ({{ dbt_utils.union_relations(relations = order_raw_tables) }}),
+
+    transactions AS 
     (SELECT 
-
         {% for column in selected_fields -%}
         {{ get_shopify_clean_field(table_name, column)}}
         {%- if not loop.last %},{% endif %}
         {% endfor %}
+    FROM transaction_raw_data
+    ),
 
-    FROM {{ source(schema_name, table_name) }}),
+    orders AS 
+    (SELECT 
+        {% for column in order_selected_fields -%}
+        {{ get_shopify_clean_field(order_table_name, column)}}
+        {%- if not loop.last %},{% endif %}
+        {% endfor %}
+    FROM order_raw_data
+    ),
+
+    {% if var('sho_aus_currency') in ['USD','AUD'] -%}
+    currency AS
+    (SELECT 
+        date,
+        currency,
+        conversion_rate
+    FROM utilities.currency
+    WHERE date <= current_date
+      AND currency = 'AUD'),
+    {%- endif -%}
 
     staging AS 
     (SELECT 
-        order_id, 
-        processed_at::date as transaction_date,
-        COALESCE(SUM(CASE WHEN kind in ('sale','authorization') THEN transaction_amount END),0)::float/{{ conversion_rate }}::float as paid_by_customer,
-        COALESCE(SUM(CASE WHEN kind = 'refund' THEN transaction_amount END),0)::float/{{ conversion_rate }}::float as refunded
-    FROM raw_table
-    {%- if var('sho_aus_currency') == 'USD' %}
-    LEFT JOIN currency ON raw_table.processed_at::date = currency.date
-    {%- endif %}
-    WHERE status = 'success'
-    GROUP BY order_id, transaction_date)
+        t.order_id, 
+        t.processed_at::date as transaction_date,
+
+        -- paid_by_customer
+        COALESCE(SUM(
+            CASE WHEN t.kind in ('sale','authorization') THEN 
+                CASE 
+                    WHEN o.currency = var('sho_aus_currency') THEN t.transaction_amount::float
+                    WHEN o.currency = 'USD' AND var('sho_aus_currency') = 'AUD' THEN t.transaction_amount::float * c.conversion_rate
+                    WHEN o.currency = 'AUD' AND var('sho_aus_currency') = 'USD' THEN t.transaction_amount::float / NULLIF(c.conversion_rate,0)
+                END
+            END
+        ),0) as paid_by_customer,
+
+        -- refunded
+        COALESCE(SUM(
+            CASE WHEN t.kind = 'refund' THEN 
+                CASE 
+                    WHEN o.currency = var('sho_aus_currency') THEN t.transaction_amount::float
+                    WHEN o.currency = 'USD' AND var('sho_aus_currency') = 'AUD' THEN t.transaction_amount::float * c.conversion_rate
+                    WHEN o.currency = 'AUD' AND var('sho_aus_currency') = 'USD' THEN t.transaction_amount::float / NULLIF(c.conversion_rate,0)
+                END
+            END
+        ),0) as refunded
+
+    FROM transactions t
+    LEFT JOIN orders o ON t.order_id = o.id
+    {% if var('sho_aus_currency') in ['USD','AUD'] %}
+        LEFT JOIN currency c ON t.processed_at::date = c.date
+    {% endif %}
+    WHERE t.status = 'success'
+    GROUP BY t.order_id, transaction_date
+    )
 
 SELECT *,
     order_id||'_'||transaction_date as unique_key
