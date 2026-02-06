8eb8ee0d4b7950633bda47b976e806b842e81128
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-02-06 17:14:09 +0100

    Update shopify_aus_daily_sales_by_order_line_item.sql

--- models/reporting/shopify_aus_daily_sales_by_order_line_item.sql
+++ models/reporting/shopify_aus_daily_sales_by_order_line_item.sql
@@ -39,10 +39,10 @@ WITH orders AS
         gift_card,
         price,
         quantity,
-        price * quantity as gross_sales,
+        pre_tax_price as gross_sales,
         discount_rate,
-        (price * quantity) * COALESCE(subtotal_revenue / NULLIF(gross_revenue,0)) as subtotal_sales,
-        (price * quantity) * COALESCE(total_revenue / NULLIF(gross_revenue,0)) as total_sales,
+        pre_tax_price * COALESCE(subtotal_revenue / NULLIF(gross_revenue,0)) as subtotal_sales,
+        pre_tax_price * COALESCE(total_revenue / NULLIF(gross_revenue,0)) as total_sales,
         quantity - COALESCE(refund_quantity,0) as net_quantity
     FROM orders 
     LEFT JOIN line_items USING(order_id)
