52a83db0ca8d8aecb42d229c7980b16a386311c5
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-07-03 11:11:08 -0300

    add columns

--- models/reporting/shopify_aus_daily_sales_by_order_line_item.sql
+++ models/reporting/shopify_aus_daily_sales_by_order_line_item.sql
@@ -1,23 +1,25 @@
 {{ config (
-    alias = target.database + '_shopify_aus_daily_sales_by_order_line_item',
+    alias = target.database + '_shopify_daily_sales_by_order_line_item',
     materialized='incremental',
-    unique_key='unique_key'
+    unique_key='unique_key',
+    on_schema_change='append_new_columns'
 )}}
 
 
 WITH orders AS 
     (SELECT *
-    FROM {{ ref('shopify_aus_daily_sales_by_order') }}
+    FROM {{ ref('shopify_daily_sales_by_order') }}
     ),
 
     line_items AS 
     (SELECT *
-    FROM {{ ref('shopify_aus_line_items') }}
+    FROM {{ ref('shopify_line_items') }}
     ),
 
-    products AS 
-    (SELECT product_id, variant_id, product_type, product_tags
-    FROM {{ ref('shopify_aus_products') }}
+    products AS
+    (SELECT product_id, product_type, product_tags, product_handle, product_status, count(*)
+    FROM {{ ref('shopify_products') }}
+    GROUP BY 1,2,3,4,5
     ),
 
     sales AS 
@@ -26,6 +28,7 @@ WITH orders AS
         cancelled_at,
         order_id, 
         customer_id,
+        customer_acquisition_date,
         customer_order_index,
         order_tags, 
         order_line_id,
@@ -39,10 +42,13 @@ WITH orders AS
         gift_card,
         price,
         quantity,
-        pre_tax_price as gross_sales,
+        line_items.fulfillment_status as item_fulfillment_status,
+        fulfillable_quantity,
+        net_subtotal,
+        price * quantity as gross_sales,
         discount_rate,
-        pre_tax_price * COALESCE(subtotal_revenue / NULLIF(gross_revenue,0)) as subtotal_sales,
-        pre_tax_price * COALESCE(total_revenue / NULLIF(gross_revenue,0)) as total_sales,
+        (price * quantity) * COALESCE(subtotal_revenue / NULLIF(gross_revenue,0)) as subtotal_sales,
+        (price * quantity) * COALESCE(total_revenue / NULLIF(gross_revenue,0)) as total_sales,
         quantity - COALESCE(refund_quantity,0) as net_quantity
     FROM orders 
     LEFT JOIN line_items USING(order_id)
@@ -50,5 +56,5 @@ WITH orders AS
 
 SELECT *,
     date||'_'||order_line_id as unique_key
-FROM sales 
-LEFT JOIN products USING(product_id, variant_id)
+FROM sales
+LEFT JOIN (SELECT product_id, product_type, product_tags, product_handle, product_status FROM products) USING(product_id)
